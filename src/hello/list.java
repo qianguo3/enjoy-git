6ca5c1e91a4ad041ab0ba2fa1803ce2763826afe
qianguo3 <77609461+qianguo3@users.noreply.github.com>
2021-05-17 09:31:22 +0800

    Update list.java

--- src/hello/list.java
+++ src/hello/list.java
@@ -13,7 +13,7 @@ public class list {
 
         try {
             list.get(0);
-            System.out.println("捕获了上合并远程仓库111111");
+            System.out.println("mster仓库更新");
 
         } catch (Exception e) {
             System.out.println("捕获了");
